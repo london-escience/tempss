ef3bc084888306cd4bd82214ae3a390c3269e6ef
Jeremy Cohen <jeremy.cohen@imperial.ac.uk>
2016-07-30 18:44:39 +0100

    Added user account checks to profile queries. Added associated UI tweaks.

--- src/main/webapp/assets/js/tempss-manager.js
+++ src/main/webapp/assets/js/tempss-manager.js
@@ -204,10 +204,14 @@ function updateProfileList(templateId) {
 // These should only be enabled when a template is selected.
 function disableProfileButtons(disable) {
 	if(disable) {
-		$('#clear-profile-btn').prop('disabled', true);	
+		$('#clear-profile-btn').prop('disabled', true);
+		$('#save-as-profile-btn').prop('disabled', true);
 	}
 	else {
 		$('#clear-profile-btn').removeProp('disabled');
+		if($('#save-as-profile-btn').attr("data-toggle") == undefined) {
+			$('#save-as-profile-btn').removeProp('disabled');
+		}
 	}
 	
 }
